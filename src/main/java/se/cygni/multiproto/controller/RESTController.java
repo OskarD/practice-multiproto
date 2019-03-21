f78273e1604315631a536b472ba7eb9874a91822
Oskar Danielsson <oskar.danielsson@tele2.com>
2019-03-21 14:35:29 +0100

    Add RESTController

---
+++ src/main/java/se/cygni/multiproto/controller/RESTController.java
@@ -0,0 +1,14 @@
+package se.cygni.multiproto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.cygni.multiproto.entity.Car;
+
+@RestController("/json")
+public class RESTController {
+
+	@RequestMapping()
+	public Car getCar() {
+		return new Car();
+	}
+}
---
+++ src/main/java/se/cygni/multiproto/entity/Car.java
@@ -0,0 +1,12 @@
+package se.cygni.multiproto.entity;
+
+@SuppressWarnings("WeakerAccess")
+public class Car {
+
+	public String name;
+
+	public Car() {
+		this.name = "Tesla";
+	}
+
+}
