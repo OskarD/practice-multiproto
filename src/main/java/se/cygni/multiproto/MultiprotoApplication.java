6532cfd26a41198b4126f0efe72d17000078759d
Oskar Danielsson <oskar.danielsson@tele2.com>
2019-03-21 10:40:15 +0100

    Initial commit

---
+++ src/main/java/se/cygni/multiproto/MultiprotoApplication.java
@@ -0,0 +1,13 @@
+package se.cygni.multiproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiprotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiprotoApplication.class, args);
+	}
+
+}
