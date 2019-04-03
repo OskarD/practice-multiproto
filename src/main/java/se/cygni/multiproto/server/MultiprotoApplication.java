7906f4f4b444d63eea9ec75fc25dfc5aca45fc5c
Oskar Danielsson <oskar.danielsson@tele2.com>
2019-04-03 12:00:48 +0200

    Move stubbing RMI service into its own constructor instead of main method

--- src/main/java/se/cygni/multiproto/server/MultiprotoApplication.java
+++ src/main/java/se/cygni/multiproto/server/MultiprotoApplication.java
@@ -2,18 +2,11 @@ package se.cygni.multiproto.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import se.cygni.multiproto.server.rmi.RMIServiceImpl;
-
-import java.rmi.RemoteException;
 
 @SpringBootApplication
 public class MultiprotoApplication {
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) {
 		SpringApplication.run(MultiprotoApplication.class, args);
-
-		RMIServiceImpl rmiService = new RMIServiceImpl();
-		rmiService.createStubAndBind();
 	}
 }
--- src/main/java/se/cygni/multiproto/server/rmi/RMIServiceImpl.java
+++ src/main/java/se/cygni/multiproto/server/rmi/RMIServiceImpl.java
@@ -12,6 +12,11 @@ import java.util.List;
 
 @Service
 public class RMIServiceImpl implements RMIService {
+
+	public RMIServiceImpl() throws RemoteException {
+		createStubAndBind();
+	}
+
 	@Override
 	public Car getCar(String name) throws UnknownCarException {
 		if (!name.equals("Tesla")) {
@@ -21,7 +26,7 @@ public class RMIServiceImpl implements RMIService {
 		return new Car("Tesla", List.of("Oskar", "Erik"));
 	}
 
-	public void createStubAndBind() throws RemoteException {
+	private void createStubAndBind() throws RemoteException {
 		Registry registry = LocateRegistry.createRegistry(1099);
 		RMIService stub = (RMIService) UnicastRemoteObject.exportObject(this, 0);
 		registry.rebind("CarService", stub);
